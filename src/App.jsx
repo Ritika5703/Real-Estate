8dd49798acc69738aef340dfdb1a147ca3f3cc03
Ritika Mishra <mishraritika579@gmail.com>
2025-01-27 19:57:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="width-full bg-gray-200">
+        <h1 className="bg-indigo-500">Real Estate</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
